114880229548314027b5f99374bb84d16b5707be
leoB4 <l.blondin4@gmail.com>
2023-03-29 16:58:22 +0200

    :sparkles: Styles - Add styles and link custom functions

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <h1>Hello</h1>
-  </div>
+  <h1>Hello</h1>
 </template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+  h1 {
+    color: colors(test-yellow);
+  }
+</style>

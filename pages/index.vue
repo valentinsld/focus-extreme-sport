ca760c20a14060114c70aab8683a561ad840611e
valentinsld <valentin.salaud@gmail.com>
2023-04-19 14:24:23 +0200

    feat(components): init game pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,51 @@
 <template>
-  <h1>Hello</h1>
+  <div style="padding: 50px;">
+    <Home v-if="store.state.gamestate === 'home'" />
+    <GameIntro v-else-if="store.state.gamestate === 'intro'" />
+    <GameWingsuit v-else-if="store.state.gamestate === 'wingsuit'" />
+    <GameSki v-else-if="store.state.gamestate === 'ski'" />
+    <GameKayak v-else-if="store.state.gamestate === 'kayak'" />
+  </div>
 </template>
 
 <script setup>
+import useStore from '@/stores/index.js'
+import WebGL from '~~/webgl';
+const store = useStore()
+
+onMounted(() => {
+  inutDebugGameState()
+})
+
+onUnmounted(() => {
+  debugFolder.dispose()
+})
+
+//
+// debug
+//
+let debugFolder
+const inutDebugGameState = () => {
+  const webgl = new WebGL()
+  debugFolder = webgl.debug.addFolder({ title: 'GameState' })
+  debugFolder.addInput(store.state, 'gamestate', {
+    options: {
+      home: 'home',
+      intro: 'intro',
+      wingsuit: 'wingsuit',
+      ski: 'ski',
+      kayak: 'kayak',
+    },
+  }).on('change', () => {
+    store.state.gamestatestep = 0
+    inputStep.refresh()
+  })
+  const inputStep = debugFolder.addInput(store.state, 'gamestatestep', {
+    min: 0,
+    max: 3,
+    step: 1,
+  })
+}
 
 </script>
 

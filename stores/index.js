f9791999994d18c72144832f01303b221367f32a
valentinsld <valentin.salaud@gmail.com>
2023-05-17 16:04:19 +0200

    Revert "style(webgl): remove console.log"
    This reverts commit b7d90ac0b9b3e1b68ca5579a9bc4f1a9ae4b3949.

--- stores/index.js
+++ stores/index.js
@@ -4,7 +4,7 @@ const useStore = defineStore('store', () => {
 	const state = reactive(
 		{
 			ressourcesLoaded: false,
-			gamestate: 'wingsuit',
+			gamestate: 'home',
 			gamestatestep: 0,
 			noEventPlayer: false,
 			altimetre: {

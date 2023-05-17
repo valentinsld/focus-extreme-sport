bab372347abb324841e1ec5e6f464778cd68cd60
valentinsld <valentin.salaud@gmail.com>
2023-05-17 15:58:29 +0200

    style(webgl): remove console.log

--- webgl/Camera.js
+++ webgl/Camera.js
@@ -315,7 +315,6 @@ export default class Camera {
   // Events
   //
   setSpeedLines() {
-    console.log('set speedlines');
     this.speedLine = new InstanciedSpeed({
       assets: this.assets,
       camera: this.current,

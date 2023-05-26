96579bd6dabbc7a09845cfc357abdd9a7b185813
leoB4 <l.blondin4@gmail.com>
2023-05-26 16:48:23 +0200

    :sparkles: Environment - Add light + hdr + new 3P cam

---
+++ webgl/Components/Environment/DirectionalLight.js
@@ -0,0 +1,94 @@
+import {
+	Object3D,
+	DirectionalLight,
+	DirectionalLightHelper
+} from 'three'
+
+export default class DirectionalLightSource {
+	constructor(options) {
+		// Set options
+		if (options) {
+			this.debug = options.debug
+			this.color = options.color
+			this.intensity = options.intensity
+			this.positions = options.positions
+			this.castShadow = options.castShadow
+			this.shadowMapSize = options.shadowMapSize
+			this.shadowBias = options.shadowBias
+		}
+
+		// Set up
+		this.container = new Object3D()
+		this.container.name = 'Directional Light'
+		this.params = {
+			color: this.color,
+			intensity: this.intensity,
+			positions: this.positions,
+			castShadow: this.castShadow,
+			shadowMapSize: this.shadowMapSize,
+			shadowBias: this.shadowBias,
+		}
+
+		this.createDirectionalLight()
+
+		// this.setDebug()
+	}
+	createDirectionalLight() {
+		this.light = new DirectionalLight(
+			this.params.color,
+			this.params.intensity
+		)
+
+		// Shadows
+		let d = 10
+		this.light.shadow.camera.near = d * 0.01
+		this.light.shadow.camera.far = 300
+		this.light.shadow.camera.right = d
+		this.light.shadow.camera.left = -d
+		this.light.shadow.camera.top = d
+		this.light.shadow.camera.bottom = -d
+		this.light.castShadow = true
+
+		this.light.castShadow = this.params.castShadow || true
+		if (this.params.shadowMapSize !== undefined) {
+			this.light.shadow.mapSize.width = this.params.shadowMapSize
+			this.light.shadow.mapSize.height = this.params.shadowMapSize
+		}
+		if (this.params.shadowBias !== undefined) {
+			this.light.shadow.bias = this.params.shadowBias
+		}
+
+		this.container.add(this.light)
+
+		this.light.target.position.set(0, 0, 0)
+
+		// Positions
+		this.light.position.x = this.params.positions.x
+		this.light.position.y = this.params.positions.y
+		this.light.position.z = this.params.positions.z
+
+		const helper = new DirectionalLightHelper(this.light, 5)
+		// this.container.add(helper)
+	}
+
+	setDebug() {
+		this.debug.setFolder(this.container.name)
+		const gui = this.debug.getFolder(this.container.name)
+
+		gui.addInput(this.light, 'position', {
+			label: 'Directional Light',
+			x: {
+				min: -10000,
+				max: 10000,
+			},
+			y: {
+				min: -10000,
+				max: 10000,
+			},
+			z: {
+				min: -10000,
+				max: 10000,
+			},
+		})
+	}
+}
--- webgl/Renderer.js
+++ webgl/Renderer.js
@@ -45,12 +45,15 @@ export default class Renderer {
     this.instance.useLegacyLights = false
     // this.instance.gammaOutPut = true
     this.instance.outputEncoding = sRGBEncoding
-    // this.instance.shadowMap.type = THREE.PCFSoftShadowMap
-    // this.instance.shadowMap.enabled = false
+    this.instance.shadowMap.type = PCFSoftShadowMap
+    this.instance.shadowMap.enabled = true
+    this.instance.shadowMapSoft = true
     this.instance.toneMapping = NoToneMapping
     this.instance.toneMappingExposure = 1
 
     this.context = this.instance.getContext()
+
+    this.generator = new PMREMGenerator(this.instance)
 
     // Debug
     if (this.debug) {

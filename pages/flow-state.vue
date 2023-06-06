f432f7114586585ca562f118cf72afed159cb705
valentinsld <valentin.salaud@gmail.com>
2023-06-06 11:20:03 +0200

    feat(app): new pages

---
+++ pages/flow-state.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page-flow">
+    <img src="/end.png">
+  </div>
+</template>
+
+<style lang="scss">
+.page-flow {
+	position: relative;
+	z-index: 1;
+
+	height: 100vh;
+	overflow-y: scroll;
+
+	display: grid;
+
+	img {
+		width: 100%;
+	}
+}
+</style>

9e0c8eb7684dbfe978c0bc6a07d1e3695ddf312d
valentinsld <valentin.salaud@gmail.com>
2023-06-06 11:20:03 +0200

    feat(app): new pages

---
+++ pages/stickers-rewards.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page-sticker">
+    <h1>Stickers-rewards</h1>
+
+    <NuxtLink to="/flow-state">
+      Go to flow state
+    </NuxtLink>
+  </div>
+</template>
+
+<style lang="scss">
+.page-sticker {
+	position: relative;
+	z-index: 1;
+}
+</style>

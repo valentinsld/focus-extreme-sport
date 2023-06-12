fe731283326d9574414ee8f23c559fc636cea68b
valentinsld <valentin.salaud@gmail.com>
2023-06-12 10:17:27 +0200

    fix(GameSki): fix

--- components/Game/Ski/GameSki.vue
+++ components/Game/Ski/GameSki.vue
@@ -49,37 +49,35 @@ onMounted(()=> {
 function initStates(scene) {
   currentScene = scene
 
-  scene.timelineValue = 0.83
+  // event QTE Balance
+  scene.setEventTimeline(0.07, () => {
+    store.state.gamestatestep = 1
+  })
 
-  // // event QTE Balance
-  // scene.setEventTimeline(0.07, () => {
-  //   store.state.gamestatestep = 1
-  // })
+  // event QTE End Balance
+  scene.setEventTimeline(0.5, () => {
+    store.state.gamestatestep = 2
+  })
 
-  // // event QTE End Balance
-  // scene.setEventTimeline(0.5, () => {
-  //   store.state.gamestatestep = 2
-  // })
+  // start travelling 3P
+  scene.setEventTimeline(0.56, () => {
+    scene.setCameraTravelling()
+  })
 
-  // // start travelling 3P
-  // scene.setEventTimeline(0.56, () => {
-  //   scene.setCameraTravelling()
-  // })
+  // end travelling 3P
+  scene.setEventTimeline(0.65, () => {
+    scene.removeCameraTravelling()
+  })
 
-  // // end travelling 3P
-  // scene.setEventTimeline(0.65, () => {
-  //   scene.removeCameraTravelling()
-  // })
+  // event QTE Focus
+  scene.setEventTimeline(0.7, () => {
+    store.state.gamestatestep = 3
+  })
 
-  // // event QTE Focus
-  // scene.setEventTimeline(0.7, () => {
-  //   store.state.gamestatestep = 3
-  // })
-
-  // // event END QTE Focus
-  // scene.setEventTimeline(0.82, () => {
-  //   store.state.gamestatestep = 4
-  // })
+  // event END QTE Focus
+  scene.setEventTimeline(0.82, () => {
+    store.state.gamestatestep = 4
+  })
 
   // event QTE Figure
   scene.setEventTimeline(0.85, () => {

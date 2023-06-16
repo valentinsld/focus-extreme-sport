39f4b30b9cf9bf5f0ab299293a2b7d6c132dd74f
leoB4 <l.blondin4@gmail.com>
2023-06-16 19:02:28 +0200

    Add lotties

--- components/Game/Ski/GameSki.vue
+++ components/Game/Ski/GameSki.vue
@@ -31,6 +31,22 @@
       ]"
       @is-finished="endQteFigure"
     />
+
+    <div
+      ref="backflipLottie"
+      class="lottie"
+      :class="{'is-visible': isBackflipVisible}"
+    />
+    <div
+      ref="balanceLottie"
+      class="lottie"
+      :class="{'is-visible': isBalanceVisible}"
+    />
+    <div
+      ref="focusLottie"
+      class="lottie"
+      :class="{'is-visible': isFocusVisible}"
+    />
   </div>
 </template>
 
@@ -38,20 +54,88 @@
 import useStore from '@/stores/index.js'
 import SceneManager from '~~/webgl/Managers/SceneManager';
 
+import lottie  from 'lottie-web'
+
+import backflip from '~~/assets/lottieJson/BACK_FLIP.json'
+import balance from '~~/assets/lottieJson/BALANCE_PINK.json'
+import focus from '~~/assets/lottieJson/STAY_FOCUS_PINK.json'
+
 const store = useStore()
+
+const backflipLottie = ref()
+const isBackflipVisible = ref(false)
+
+const balanceLottie = ref()
+const isBalanceVisible = ref(false)
+
+const focusLottie = ref()
+const isFocusVisible = ref(false)
 
 let currentScene = null
 let doBackFlip = null
+
+let backflipAnime, balanceAnime, focusAnime;
+
 onMounted(()=> {
   const sceneManager = new SceneManager()
   sceneManager.setScene('ski', 0.35, initStates)
+
+  initLottie();
 })
+
+function initLottie() {
+  backflipAnime = lottie.loadAnimation({
+    container: backflipLottie.value, // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    animationData: backflip,
+  });
+
+  backflipAnime.onComplete = function(){
+    isBackflipVisible.value = false
+  }
+
+  balanceAnime = lottie.loadAnimation({
+    container: balanceLottie.value, // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    animationData: balance,
+  });
+
+  balanceAnime.onComplete = function(){
+    isBalanceVisible.value = false
+  }
+
+  focusAnime = lottie.loadAnimation({
+    container: focusLottie.value, // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    animationData: focus,
+  });
+
+  focusAnime.onComplete = function(){
+    isFocusVisible.value = false
+  }
+}
+
 
 function initStates(scene) {
   currentScene = scene
 
-  // event QTE Balance
+  // Show lottie balance
+  scene.setEventTimeline(0.095, () => {
+    isBalanceVisible.value = true
+  })
   scene.setEventTimeline(0.1, () => {
+    balanceAnime.play()
+  })
+
+
+  // event QTE Balance
+  scene.setEventTimeline(0.175, () => {
     store.state.gamestatestep = 1
   })
 
@@ -68,19 +152,36 @@ function initStates(scene) {
   })
 
   // end travelling 3P
-  scene.setEventTimeline(0.65, () => {
+  scene.setEventTimeline(0.6, () => {
     scene.removeCameraTravelling()
   })
 
+  // Show lottie focus
+  scene.setEventTimeline(0.615, () => {
+    isFocusVisible.value = true
+  })
+  scene.setEventTimeline(0.62, () => {
+    focusAnime.play()
+  })
+
   // event QTE Focus
-  scene.setEventTimeline(0.7, () => {
+  scene.setEventTimeline(0.65, () => {
     store.state.gamestatestep = 3
   })
 
   // event END QTE Focus
-  scene.setEventTimeline(0.82, () => {
+  scene.setEventTimeline(0.78, () => {
     store.state.gamestatestep = 4
   })
+
+  // Show lottie focus
+  scene.setEventTimeline(0.795, () => {
+    isBackflipVisible.value = true
+  })
+  scene.setEventTimeline(0.8, () => {
+    backflipAnime.play()
+  })
+
 
   // event QTE Figure
   scene.setEventTimeline(0.845, () => {
@@ -121,3 +222,24 @@ const endQteFigure = (isSucess) => {
   doBackFlip = isSucess
 }
 </script>
+
+<style lang="scss" scoped>
+.game-ski {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  position: relative;
+}
+
+.lottie {
+  position: absolute;
+  width: 120rem;
+  transition: opacity .3s ease(out-swift);
+  opacity: 0;
+
+  &.is-visible {
+    opacity: 1;
+  }
+}
+</style>

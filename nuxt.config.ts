34f8c42d37e6377551d2f7c4c002ef5e7bb2f2fa
valentinsld <valentin.salaud@gmail.com>
2023-06-20 17:29:44 +0200

    feat(favicon): new favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,20 @@ import glsl from 'vite-plugin-glsl'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	app: {
+		head: {
+			title: 'Focus | A la poursuite de l\'état',
+			meta: [
+				{ charset: 'utf-8' },
+				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+				{ hid: 'description', name: 'description', content: 'Une expérience documentaire intéractive au coeur des sports d’actions.' }
+			],
+			link: [
+				{ rel: 'icon', type: 'image/x-icon', href: '/favicon-dark.ico', media: "(prefers-color-scheme: light)" },
+				{ rel: 'icon', type: 'image/x-icon', href: '/favicon-white.ico', media: "(prefers-color-scheme: dark)" }
+			]
+		},
+	},
 	vite: {
 		plugins: [
 			eslintPlugin(),

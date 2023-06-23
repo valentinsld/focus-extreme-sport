0c5710ab60bd90f526ff702dedc0970a549dfcbe
leoB4 <l.blondin4@gmail.com>
2023-06-23 18:42:24 +0200

    Add footer

--- components/End/Footer/EndFooter.vue
+++ components/End/Footer/EndFooter.vue
@@ -1,39 +1,136 @@
 <template>
-  <div>
-    <img src="/flow-state/footer/gobelins.png">
-    <div>
-      <p>
+  <footer class="footer">
+    <img
+      class="gobelins"
+      src="/flow-state/footer/gobelins.png"
+    >
+    <div class="footer-content">
+      <p class="initialisation">
         Ce projet est réalisé dans le cadre du Master 2 - DMII
       </p>
-      <p>
+      <p class="links">
         par
         <a
           href="http://vaninaidiart.fr/"
           target="_blank"
-        >Vanina IDIART</a>,
+        ><span>Vanina IDIART</span></a>,
         <a
           href="https://www.behance.net/tanguypuech"
           target="_blank"
-        >Tanguy PUECH</a>,
+        ><span>Tanguy PUECH</span></a>,
         <a
           href="http://www.leoblondin.com/"
           target="_blank"
-        >Léo BLONDIN</a>,
+        ><span>Léo BLONDIN</span></a>,
         <a
           href="https://www.valentinsld.fr/"
           target="_blank"
-        >Valentin SALAUD</a>,
+        ><span>Valentin SALAUD</span></a>,
         <a
           href="https://www.behance.net/axelcrb"
           target="_blank"
-        >Axel CROUILBOIS</a> et
+        ><span>Axel CROUILBOIS</span></a> et
         <a
           href="https://www.behance.net/margauxclm6c96"
           target="_blank"
-        >Margaux CLEMENT</a>
+        ><span>Margaux CLEMENT</span></a>
       </p>
     </div>
 
-    <img src="/flow-state/footer/cci.png">
-  </div>
+    <img
+      class="cci"
+      src="/flow-state/footer/cci.png"
+    >
+  </footer>
 </template>
+
+<style lang="scss" scoped>
+.footer {
+  position: relative;
+  z-index: 1;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  max-width: 980px;
+  margin: auto;
+  padding: 5.5rem 0;
+
+  border-top: 1px solid colors(black);
+
+  .gobelins {
+    width: 8.5rem;
+  }
+
+  .footer-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    color: colors(black);
+
+    .initialisation {
+      font-family: const(font-gotham);
+      font-weight: 400;
+      font-size: 1.5rem;
+      text-align: center;
+    }
+
+    .links {
+      max-width: 60%;
+      font-family: const(font-gotham);
+      font-weight: 400;
+      font-size: 1.5rem;
+      text-align: center;
+      color: rgba(colors(black), .5);
+
+      a {
+        color: rgba(colors(black), .5);
+        white-space: nowrap;
+        line-height: 140%;
+        transition: color .3s ease(out-swift);
+
+        span {
+          position: relative;
+          z-index: 1;
+
+          &::after {
+            content: "";
+            position: absolute;
+            display: block;
+            width: 100%;
+            height: 1px;
+            background-color: rgba(colors(black), .5);
+            bottom: -2px;
+            left: 0;
+            transform: scaleX(0);
+            transform-origin: center right;
+            transition: transform .3s ease(out-swift), background-color .3s ease(out-swift);
+            will-change: transform;
+          }
+        }
+
+        &:hover {
+          color: colors(f_purple);
+
+          span {
+            &::after {
+              transform: scaleX(1);
+              transform-origin: center left;
+              background-color: colors(f_purple);
+            }
+          }
+        }
+
+      }
+    }
+  }
+
+  .cci {
+    width: 8.5rem;
+  }
+}
+</style>

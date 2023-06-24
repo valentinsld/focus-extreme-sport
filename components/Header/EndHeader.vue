204b57caffc74c883bea86f91ab7a9e93d049848
leoB4 <l.blondin4@gmail.com>
2023-06-24 17:19:58 +0200

    Start header

---
+++ components/Header/EndHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <header class="header">
+    <NuxtLink
+      to="/"
+    >
+      <span>
+        home
+      </span>
+    </NuxtLink>
+
+    <nav class="nav">
+      <a href="#flow"><span>Le flow</span></a>
+      <div class="separator" />
+      <a href="#map"><span>Parcours</span></a>
+      <div class="separator" />
+      <a href="#athletes"><span>Athlètes</span></a>
+      <div class="separator" />
+      <a href="#sports"><span>Sports</span></a>
+      <div class="separator" />
+      <a href="#event"><span>Évènement</span></a>
+    </nav>
+
+
+    <NuxtLink
+      to="/stickers-rewards"
+    >
+      <span>
+        stickers
+      </span>
+    </NuxtLink>
+  </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.header {
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 2rem 4rem;
+  width: 100vw;
+}
+</style>

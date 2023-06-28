883373f8bd6c22e4565c46a3c9cce817e36ffb8a
leoB4 <l.blondin4@gmail.com>
2023-06-28 15:01:28 +0200

    Add infos stickers + new clouds

--- components/Stickers/Rewards/Slider.vue
+++ components/Stickers/Rewards/Slider.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="rewards-slider">
+  <div
+    ref="base"
+    class="rewards-slider"
+  >
     <button
       class="rewards-slider__button"
       @click="slider.prev()"
@@ -27,6 +30,9 @@
           'keen-slider__slide': true,
           'is-hide': !stickers.state.like_a_boss,
         }"
+        data-sport="boss"
+        @mouseenter="showInfos"
+        @mouseleave="hideInfos"
       >
         <img
           src="/stickers/like_a_boss.png"
@@ -37,6 +43,10 @@
           'keen-slider__slide': true,
           'is-hide': !stickers.state.wingsuit.includes(1)
         }"
+        data-sport="wingsuit"
+        data-level="top"
+        @mouseenter="showInfos"
+        @mouseleave="hideInfos"
       >
         <img
           src="/stickers/1-wingsuit.png"
@@ -47,6 +57,10 @@
           'keen-slider__slide': true,
           'is-hide': !stickers.state.wingsuit.includes(2)
         }"
+        data-sport="wingsuit"
+        data-level="mid"
+        @mouseenter="showInfos"
+        @mouseleave="hideInfos"
       >
         <img
           src="/stickers/2-wingsuit.png"
@@ -57,6 +71,10 @@
           'keen-slider__slide': true,
           'is-hide': !stickers.state.wingsuit.includes(3)
         }"
+        data-sport="wingsuit"
+        data-level="bot"
+        @mouseenter="showInfos"
+        @mouseleave="hideInfos"
       >
         <img
           src="/stickers/3-wingsuit.png"
@@ -67,6 +85,10 @@
           'keen-slider__slide': true,
           'is-hide': !stickers.state.ski.includes(1)
         }"
+        data-sport="ski"
+        data-level="top"
+        @mouseenter="showInfos"
+        @mouseleave="hideInfos"
       >
         <img
           src="/stickers/1-ski.png"
@@ -77,6 +99,10 @@
           'keen-slider__slide': true,
           'is-hide': !stickers.state.ski.includes(2)
         }"
+        data-sport="ski"
+        data-level="mid"
+        @mouseenter="showInfos"
+        @mouseleave="hideInfos"
       >
         <img
           src="/stickers/2-ski.png"
@@ -87,6 +113,10 @@
           'keen-slider__slide': true,
           'is-hide': !stickers.state.ski.includes(3)
         }"
+        data-sport="ski"
+        data-level="bot"
+        @mouseenter="showInfos"
+        @mouseleave="hideInfos"
       >
         <img
           src="/stickers/3-ski.png"
@@ -97,6 +127,10 @@
           'keen-slider__slide': true,
           'is-hide': !stickers.state.kayak.includes(1)
         }"
+        data-sport="kayak"
+        data-level="top"
+        @mouseenter="showInfos"
+        @mouseleave="hideInfos"
       >
         <img
           src="/stickers/1-kayak.png"
@@ -107,6 +141,10 @@
           'keen-slider__slide': true,
           'is-hide': !stickers.state.kayak.includes(2)
         }"
+        data-sport="kayak"
+        data-level="mid"
+        @mouseenter="showInfos"
+        @mouseleave="hideInfos"
       >
         <img
           src="/stickers/2-kayak.png"
@@ -117,6 +155,10 @@
           'keen-slider__slide': true,
           'is-hide': !stickers.state.kayak.includes(3)
         }"
+        data-sport="kayak"
+        data-level="bot"
+        @mouseenter="showInfos"
+        @mouseleave="hideInfos"
       >
         <img
           src="/stickers/3-kayak.png"
@@ -140,6 +182,16 @@
         />
       </svg>
     </button>
+    <div
+      class="stickers-infos"
+      :class="{'is-visible': isInfosVisible}"
+    >
+      <div class="animate-infos">
+        <div class="infos-text">
+          <p v-text="textInfos" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -148,6 +200,17 @@ import { useKeenSlider } from 'keen-slider/vue.es'
 import 'keen-slider/keen-slider.min.css'
 import WheelControls from '~/js/slider-plugin/wheel.js'
 import useStickers from '~~/stores/stickers'
+import { useMouseInElement } from '@vueuse/core'
+import RAFManager from '~~/webgl/Utils/RAFManager';
+import { clampedMap } from '~~/webgl/Utils/Math';
+import { lerp } from '~~/webgl/Utils/Lerp';
+
+const textInfos = ref()
+const base = ref()
+const isInfosVisible = ref(false)
+
+let y = 0;
+let lerpY = 0
 
 const stickers = useStickers()
 
@@ -168,10 +231,136 @@ const [container, slider] = useKeenSlider({
 
 			// wallah ca fonctionne, demander à Valentin
 			const op = 1 - ((Math.abs(slide.distance / 0.84 - 0.5)) - 0.34) * 6.25
+      if(op <= 0.01) {
+        slideEl.classList.add('not-visible')
+      } else if(op !== 0 && slideEl.classList.contains('not-visible')) {
+        slideEl.classList.remove('not-visible')
+      }
 			slideEl.style.opacity = op
 		});
 	},
 }, [WheelControls])
+
+const infosContent = {
+  boss: {
+    textLocked: "Débloque-le en gagnant tous les stickers",
+    textUnlocked: "Le boss des boss, tu as gagné tous les stickers!"
+  },
+  wingsuit: {
+    top: {
+      textLocked: "Débloque-le en réussissant toutes les actions du wingsuit",
+      textUnlocked: "3/3 de réussite sur le wingsuit"
+    },
+    mid: {
+      textLocked: "Débloque-le en réussissant 2 actions du wingsuit",
+      textUnlocked: "2/3 de réussite sur le wingsuit"
+    },
+    bot: {
+      textLocked: "Locked",
+      textUnlocked: "1/3 de réussite sur le wingsuit"
+    }
+  },
+  ski: {
+    top: {
+      textLocked: "Débloque-le en réussissant toutes les actions du freeride",
+      textUnlocked: "3/3 de réussite sur le freeride"
+    },
+    mid: {
+      textLocked: "Débloque-le en réussissant 2 interactions du freeride",
+      textUnlocked: "2/3 de réussite sur le freeride"
+    },
+    bot: {
+      textLocked: "Locked",
+      textUnlocked: "1/3 de réussite sur le freeride"
+    }
+  },
+  kayak: {
+    top: {
+      textLocked: "Débloque-le en réussissant toutes les actions du kayak",
+      textUnlocked: "3/3 de réussite sur le kayak"
+    },
+    mid: {
+      textLocked: "Débloque-le en réussissant 2 interactions du kayak",
+      textUnlocked: "2/3 de réussite sur le kayak"
+    },
+    bot: {
+      textLocked: "Locked",
+      textUnlocked: "1/3 de réussite sur le kayak"
+    }
+  }
+}
+
+function showInfos(e) {
+
+  const sport = e.target.dataset.sport
+  const level = e.target.dataset.level
+  let index;
+
+  switch (e.target.dataset.level) {
+    case "top":
+      index = 1
+      break;
+
+    case "mid":
+      index = 2
+      break;
+
+    case "bot":
+      index = 3
+      break;
+  }
+
+  if (sport !== "boss" && stickers.state[sport].includes(index)) {
+    textInfos.value = infosContent[sport][level].textUnlocked
+  } else if(sport !== "boss" && !stickers.state[sport].includes(index)) {
+    textInfos.value = infosContent[sport][level].textLocked
+  }
+
+  if(sport === 'boss' && stickers.state.like_a_boss) {
+    textInfos.value = infosContent[sport].textUnlocked
+  } else if(sport === 'boss' && !stickers.state.like_a_boss) {
+    textInfos.value = infosContent[sport].textLocked
+  }
+
+
+  if(!e.target.classList.contains('not-visible')) {
+    startHover()
+    isInfosVisible.value = true
+  }
+}
+
+function hideInfos() {
+  isInfosVisible.value = false
+  removeHover()
+}
+
+function startHover() {
+    const {
+      elementY,
+      elementHeight
+	  } = useMouseInElement(base.value);
+
+    const element = base.value.querySelector('.stickers-infos');
+
+
+    RAFManager.add('stickers-infos', () => {
+      update(elementY, elementHeight, element)
+    })
+  }
+
+  function removeHover() {
+		RAFManager.remove('stickers-infos')
+	}
+
+  function update( elY, elH, element) {
+
+    y = clampedMap(elY.value, 0, elH.value, 0, elY.value);
+
+    lerpY = lerp(lerpY, y, .1)
+
+    element.style.transform = `translateY(${lerpY}px)`;
+
+  }
 </script>
 
 <style lang="scss">
@@ -281,6 +470,46 @@ const [container, slider] = useKeenSlider({
     &:active {
       transform: scale(0.75);
     }
+  }
+}
+
+.stickers-infos {
+  position: absolute;
+  right: -100%;
+  top: 20%;
+  z-index: 3;
+}
+
+.animate-infos {
+  .is-visible & {
+    animation: softBounceOut .5s linear 1 forwards;
+  }
+}
+
+.infos-text {
+  background-color: #EFF3F5;
+  padding: .5rem 1rem;
+  width: 15rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.15);
+  opacity: 0;
+  transform: rotate(10deg) scale(.25);
+  transition: transform .5s ease(out-bounce), opacity .4s ease(out-swift);
+
+  .is-visible & {
+    opacity: 1;
+    transform: rotate(0deg) scale(1);
+  }
+
+  p {
+
+    text-align: center;
+    font-family: const(font-gotham);
+    font-weight: 400;
+    color: colors(black);
+    font-size: 1.3rem;
   }
 }
 </style>
